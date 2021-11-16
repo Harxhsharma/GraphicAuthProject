43763145fd7259b885c6c3954fe5bef1d4b690b8
Harsh Sharma <76914718+Harxhsharma@users.noreply.github.com>
2021-11-16 16:14:55 +0530

    Update settings.py

--- mysite/settings.py
+++ mysite/settings.py
@@ -123,6 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS=[os.path.join(BASE_DIR, 'static')]
+STATIC_ROOT =os.path.join(BASE_DIR, 'assets')
 
 
 # Default primary key field type
@@ -130,7 +132,8 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-
+MEDIA_URL='/media/'
+MEDIA_ROOT=os.path.join(BASE_DIR,'media')
 
 
 EMAIL_HOST='smtp.gmail.com'
